ad1f0fbcc99b951656845391bccc5e18716c1de6
Zisan-uzum <uzummzisann@gmail.com>
2022-06-18 12:15:43 +0300

    dont merge this branch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./style.css";
+import "./components/Navbar";
 
 export default function App() {
-  return (
-    <div>
-      <h1>React Ecommerce!</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1>React Ecommerce!</h1>
+            <Navbar></Navbar>
+        </div>
+    );
 }
---
+++ src/components/navbar.jsx
@@ -0,0 +1,15 @@
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import PersonIcon from "@mui/icons-material/Person";
+function Navbar() {
+    return (
+        <div>
+            <h1>Ninjas Store</h1>;<button>name</button>
+            <button>price</button>
+            <button>category</button>
+            <button>rating</button>
+            {/* <ShoppingCartIcon></ShoppingCartIcon>
+            <p>CART</p>
+            <PersonIcon></PersonIcon> */}
+        </div>
+    );
+}
